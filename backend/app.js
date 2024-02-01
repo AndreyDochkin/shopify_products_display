414c93a1e31e36196a6a97c9ee94ed04c8de77c1
Andrei Dochkin <andrey.dochkin@gmail.com>
2024-02-01 20:04:21 +0400

    feat: app main

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,28 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const { PORT, MONGO_URI } = require("./config");
+const database = require("./database"); // Database connection setup
+const cors = require("cors");
+const helmet = require("helmet");
+const limiterConfig = require("./utils/rateLimitConfig"); // Rate limiting to prevent abuse
+const corsConfig = require("./utils/corsConfig"); // CORS configuration for cross-origin requests
+const productRoutes = require("./routes/products"); // Routes for product operations
+const { fetchAndSaveProducts } = require("./controllers/products"); // Function to fetch products from Shopify and save to DB
+const { PORT } = require("./config");
 
 const app = express();
 
-app.use(cors(corsConfig)); // Apply CORS
-app.use(helmet()); // Set security headers
-app.use(limiterConfig); // Apply rate limiting
-
+// Middleware setup
+app.use(cors(corsConfig));
+app.use(helmet());
+app.use(limiterConfig);
 app.use(express.json());
 
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => console.error(err));
+// Routing
+app.use("/", productRoutes);
 
+database(); // Initialize database connection
+
+// Start the server and fetch initial data from external api
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
+  fetchAndSaveProducts(); //
 });
